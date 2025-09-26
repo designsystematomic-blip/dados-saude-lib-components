1a4fa3cdb075c98cebc4ee3fb0f0e48dfb8ec63c
jubilly <amandaprates-12@hotmail.com>
2025-09-25 22:47:21 -0300

    chore: clean project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { Form } from "../lib/components";
-
 function App() {
   return (
     <>
       <h1>Dados Saúde Lib Components</h1>
       <p>Welcome to the Dados Saúde Lib Components library!</p>
-      <Form.Input />
     </>
   );
 }
